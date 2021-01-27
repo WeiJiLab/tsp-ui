d28740b888b81c305d0d2fdd876263d7a71a5ca4
NeoYang <nerosoft@outlook.com>
2021-01-27 13:48:47 +0800

    [XingfengYang] #VMM: bug

--- src/pages/dashboard/DashBoard.js
+++ src/pages/dashboard/DashBoard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Container, Row} from "react-bootstrap";
-import {Area} from "@ant-design/charts";
+import {Area, Pie} from "@ant-design/charts";
 import './DashBoard.css'
 import Card from "../../components/card/Card";
 
@@ -384,17 +384,63 @@ var config = {
     seriesField: 'country',
 };
 
+var pieConfig = {
+    appendPadding: 10,
+    data: [
+        {
+            type: '分类一',
+            value: 27,
+        },
+        {
+            type: '分类二',
+            value: 25,
+        },
+        {
+            type: '分类三',
+            value: 18,
+        },
+        {
+            type: '分类四',
+            value: 15,
+        },
+        {
+            type: '分类五',
+            value: 10,
+        },
+        {
+            type: '其他',
+            value: 5,
+        },
+    ],
+    angleField: 'value',
+    colorField: 'type',
+    radius: 0.9,
+    label: {
+        type: 'inner',
+        offset: '-30%',
+        content: function content(_ref) {
+            var percent = _ref.percent;
+            return ''.concat(percent * 100, '%');
+        },
+        style: {
+            fontSize: 14,
+            textAlign: 'center',
+        },
+    },
+    interactions: [{type: 'element-active'}],
+};
+
 class DashBoard extends React.Component {
     render() {
         return <Container className={'DashBoard'}>
             <Row style={{padding: 0, margin: 0}}>
-                <Card title={'图表0'} w={3}>
-                    <Area {...config} />
+                <Card title={'圆圆的图表'} w={3}>
+                    <Pie {...pieConfig}/>
                 </Card>
-                <Card title={'这是个卡片'} w={3}>
+                <Card title={'卡片'} w={3}>
                     <p>这个卡片里面啥都没有</p>
                 </Card>
-                <Card title={'图表2'} w={6}>
+                <Card title={'线条的图表'} w={6}>
                     <Area {...config} />
                 </Card>
             </Row>
