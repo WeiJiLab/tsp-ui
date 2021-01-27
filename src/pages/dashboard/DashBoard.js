90154fafba7be45e17820edca718d561a1bd68d3
NeoYang <nerosoft@outlook.com>
2021-01-27 14:10:32 +0800

    [XingfengYang] #N/A

--- src/pages/dashboard/DashBoard.js
+++ src/pages/dashboard/DashBoard.js
@@ -1,11 +1,17 @@
 import React from "react";
-import {Container} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
 import './DashBoard.css'
+import Card from "../../components/card/Card";
 
 class DashBoard extends React.Component {
     render() {
         return <Container className={'DashBoard'}>
-            dashboard
+            <Row style={{padding: 0, margin: 0}}>
+                <Card title={'卡片'} w={3}>这是个卡片</Card>
+                <Card title={'卡片'} w={3}>这是个卡片</Card>
+                <Card title={'卡片'} w={3}>这是个卡片</Card>
+                <Card title={'卡片'} w={3}>这是个卡片</Card>
+            </Row>
         </Container>
     }
 }
