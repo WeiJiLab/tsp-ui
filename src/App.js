8446e306d760c941a6e5c2d3cba5641764e4bade
NeoYang <nerosoft@outlook.com>
2021-01-27 09:48:07 +0800

    [XingfengYang] #N/A: left meu

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ScanList from "./pages/scan-list/ScanList";
 import ScanResult from "./pages/scan-result/ScanResult";
 import DashBoard from "./pages/dashboard/DashBoard";
 import {faCheckCircle, faCircle, faDotCircle, faHome} from "@fortawesome/free-solid-svg-icons";
+import {Component} from "react";
+import BreadCrumbMenu from "./components/breadcrumb-menu/BreadCrumbMenu";
 
 const menuData = [
     {
@@ -85,53 +87,80 @@ const menuData = [
     }
 ];
 
-function App() {
-    return (
-        <Container className="App" style={{padding: 0}}>
-            <Container style={{margin: 0, padding: 0}}>
-                <Row style={{margin: 0, padding: 0}}>
-                    <Col md={2} style={{margin: 0, padding: 0, paddingLeft: '2em', paddingTop: '1em', background: 'transparent'}}>
-                        <Container style={{padding: 0, margin: 0, borderBottom: 'solid 1px rgb(55,83,182)', height: '4em'}}>
-                            {/*<Image style={{width: '2em', height: '2em', display: 'inline-block'}}*/}
-                            {/*       src={'http://midone.left4code.com/dist/images/logo.svg'}/>*/}
-                            <p style={{
-                                color: '#fff',
-                                display: 'inline-block',
-                                height: '2em',
-                                lineHeight: '2em',
-                                padding: 0,
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            breadcrumb: [
+                {
+                    title: 'Application',
+                    clickable: false,
+                    route: '/'
+                },
+                {
+                    title: 'Application2',
+                    clickable: true,
+                    route: '/'
+                }
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <Container className="App" style={{padding: 0}}>
+                <Container style={{margin: 0, padding: 0}}>
+                    <Row style={{margin: 0, padding: 0}}>
+                        <Col md={2} style={{margin: 0, padding: 0, paddingLeft: '2em', paddingTop: '1em', background: 'transparent'}}>
+                            <Container style={{padding: 0, margin: 0, borderBottom: 'solid 1px rgb(55,83,182)', height: '4em'}}>
+                                {/*<Image style={{width: '2em', height: '2em', display: 'inline-block'}}*/}
+                                {/*       src={'http://midone.left4code.com/dist/images/logo.svg'}/>*/}
+                                <p style={{
+                                    color: '#fff',
+                                    display: 'inline-block',
+                                    height: '4em',
+                                    lineHeight: '1.5em',
+                                    padding: 0,
+                                    margin: 0,
+                                    fontWeight: 'bolder',
+                                    fontSize: '2em',
+                                }}>Αιγίς</p>
+                            </Container>
+                            <LeftMenu menu={menuData}/>
+                        </Col>
+                        <Col md={10} style={{margin: 0, padding: 0, paddingRight: '2em', paddingTop: '1em'}}>
+                            <Container style={{
                                 margin: 0,
-                                fontWeight: 'bolder',
-                                fontSize: '2em',
-                            }}>Αιγίς</p>
-                        </Container>
-                        <LeftMenu menu={menuData}/>
-                    </Col>
-                    <Col md={10} style={{margin: 0, padding: 0, paddingRight: '2em', paddingTop: '1em'}}>
-                        <Container style={{
-                            margin: 0,
-                            padding: '1em',
-                            height: '4em',
-                            borderBottom: 'solid 1px #eee',
-                            background: 'rgb(242, 245, 248)',
-                            borderTopLeftRadius: '2em',
-                            borderTopRightRadius: '2em'
-                        }}>
-                            <p style={{height: '3em', lineHeight: '3em'}}>Application > </p>
-                        </Container>
-                        <Container
-                            style={{margin: 0, padding: '1em', background: 'rgb(242, 245, 248)', borderBottomLeftRadius: '2em', borderBottomRightRadius: '2em'}}>
-                            <Switch>
-                                <Route exact path={'/'} component={DashBoard}/>
-                                <Route exact path={'/ScanList'} component={ScanList}/>
-                                <Route exact path={'/ScanResult'} component={ScanResult}/>
-                            </Switch>
-                        </Container>
-                    </Col>
-                </Row>
+                                padding: '1em',
+                                height: '4em',
+                                borderBottom: 'solid 1px #dedede',
+                                background: 'rgb(242, 245, 248)',
+                                borderTopLeftRadius: '2em',
+                                borderTopRightRadius: '2em'
+                            }}>
+                                <BreadCrumbMenu data={this.state.breadcrumb}/>
+                            </Container>
+                            <Container
+                                style={{
+                                    margin: 0,
+                                    padding: '1em',
+                                    background: 'rgb(242, 245, 248)',
+                                    borderBottomLeftRadius: '2em',
+                                    borderBottomRightRadius: '2em'
+                                }}>
+                                <Switch>
+                                    <Route exact path={'/'} component={DashBoard}/>
+                                    <Route exact path={'/ScanList'} component={ScanList}/>
+                                    <Route exact path={'/ScanResult'} component={ScanResult}/>
+                                </Switch>
+                            </Container>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
-        </Container>
-    );
+        );
+    }
 }
 
 export default App;
---
+++ src/components/breadcrumb-menu/BreadCrumbMenu.js
@@ -0,0 +1,23 @@
+import {Component} from "react";
+import {Breadcrumb} from "react-bootstrap";
+
+import './BreadCrumbMenu.css'
+
+class BreadCrumbMenu extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <Breadcrumb className={'BC'}>
+            {
+                this.props.data.map((item, index) => {
+                    return <Breadcrumb.Item active={!item.clickable} href={item.route}>{item.title}</Breadcrumb.Item>
+                })
+            }
+        </Breadcrumb>
+    }
+}
+
+export default BreadCrumbMenu;
