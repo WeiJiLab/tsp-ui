edd5453babf4d4134916f68d677f68045cdd4aec
NeoYang <nerosoft@outlook.com>
2021-01-31 00:37:08 +0800

    [XingfengYang] #Fix: list iter

--- src/pages/dashboard/DashBoard.js
+++ src/pages/dashboard/DashBoard.js
@@ -1,12 +1,25 @@
 import React from "react";
-import {Container, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import './DashBoard.css'
 import Card from "../../components/card/Card";
 import {bindActionCreators} from "redux";
 import {getProjects} from "../../actions/ProjectsAction";
 import {connect} from "react-redux";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCube, faCubes, faFileArchive, faHistory} from "@fortawesome/free-solid-svg-icons";
+import {
+    faCheckCircle,
+    faCube,
+    faCubes,
+    faEject,
+    faFileArchive,
+    faFileSignature,
+    faHistory,
+    faPlayCircle,
+    faShieldAlt,
+    faSpinner,
+    faStopCircle,
+    faTimesCircle
+} from "@fortawesome/free-solid-svg-icons";
 import {Link} from "react-router-dom";
 import {getTools} from "../../actions/ToolsAction";
 import {setBreadCrumbMenu} from "../../actions/BreadCrumbMenuAction";
@@ -151,13 +164,85 @@ class DashBoard extends React.Component {
             </Row>
             <Row style={{padding: 0, margin: 0, marginTop: '2em'}}>
                 <Card title={'最近的扫描'} w={6}>
-                    <span style={{color: '#aaa'}}>无</span>
+                    <Container>
+                        <Row>
+                            {
+                                (this.props.tasks && this.props.tasks.length !== 0) ? this.props.tasks.map((task, index) => {
+                                    return this.renderTaskRow(task, index);
+                                }) : <span style={{color: '#668'}}>无</span>
+                            }
+                        </Row>
+                    </Container>
                 </Card>
                 <Card title={'待扫描的任务'} w={6}>
-                    <span style={{color: '#aaa'}}>无</span>
+                    <Container>
+                        <Row>
+                            {
+                                (this.props.tasks && this.props.tasks.length !== 0) ? this.props.tasks.map((task, index) => {
+                                    if (task.status === 'READY') {
+                                        return this.renderTaskRow(task, index);
+                                    }
+                                }) : <span style={{color: '#668'}}>无</span>
+                            }
+                        </Row>
+                    </Container>
                 </Card>
             </Row>
         </Container>
+    }
+
+    renderTaskRow(task, index) {
+        return <Container className={'AppItem'} key={index}>
+            <Row style={{paddingTop: '0.5em', paddingBottom: '0.5em', borderBottom: 'solid 1px #f3f3f3'}}>
+                <Col md={11}>
+                    <Row>
+                        <Col md={6}>
+                            <h3><FontAwesomeIcon style={{color: 'rgb(36, 66, 164)'}} icon={faCubes}/>&nbsp;
+                                <Link to={'/project/' + task.application.projectId + '/' + task.application.id}>{task.application.name}</Link>
+                            </h3>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6}>
+                            <h5><FontAwesomeIcon style={{color: 'rgb(36, 66, 164)'}} icon={faShieldAlt}/>&nbsp;
+                                <Link to={'/tool/' + task.securityTool.id}>{task.securityTool.name}</Link>
+                            </h5>
+                        </Col>
+                        <Col md={6}>
+                            <h5><FontAwesomeIcon style={{color: 'rgb(36, 66, 164)'}} icon={faFileSignature}/>&nbsp;
+                                <Link to={'/case/' + task.useCaseEntity.id}>{task.useCaseEntity.name}</Link>
+                            </h5>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col md={1} style={{paddingTop: '0.7em'}}>
+                    <Row>
+                        {this.renderStatus(task.status)}
+                    </Row>
+                    <Row style={{marginTop: '0.3em'}}>
+                        <span style={{fontSize: '0.77em'}}>{task.startTime ? task.startTime : '未开始'}</span>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>;
+    }
+
+    renderStatus(status) {
+        switch (status) {
+            case 'READY':
+                return <FontAwesomeIcon style={{color: 'rgb(36, 66, 164)', fontSize: '2em'}} icon={faPlayCircle}/>;
+            case 'RUNNING':
+                return <FontAwesomeIcon style={{color: 'rgb(36, 66, 164)', fontSize: '2em'}} icon={faSpinner}/>;
+            case 'DONE':
+                return <FontAwesomeIcon style={{color: 'rgb(36, 66, 164)', fontSize: '2em'}} icon={faCheckCircle}/>;
+            case 'FAILED':
+                return <FontAwesomeIcon style={{color: 'rgb(36, 66, 164)', fontSize: '2em'}} icon={faTimesCircle}/>;
+            case 'ABORT':
+                return <FontAwesomeIcon style={{color: 'rgb(36, 66, 164)', fontSize: '2em'}} icon={faStopCircle}/>;
+            default:
+                return <FontAwesomeIcon style={{color: 'rgb(36, 66, 164)', fontSize: '2em'}} icon={faEject}/>;
+        }
+
     }
 
     componentDidMount() {
