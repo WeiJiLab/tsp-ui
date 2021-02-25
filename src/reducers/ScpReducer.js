14d636718c2cdd36eb2922a7d634137441d6c518
Xinpeng Wang <xinpeng.wang@TW-MacBook-Pro.local>
2021-02-25 12:28:33 +0800

    revert unused code

--- src/reducers/ScpReducer.js
+++ src/reducers/ScpReducer.js
@@ -141,67 +141,88 @@ export function reduxResult(state = initialState, action) {
                 apps: applications,
             });
         case ProjectsActions.API_PROJECTS_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                projects: action.payload,
+                apps: [],
+            });
         case ToolsActions.API_TOOLS_SUCCESS:
             return Object.assign({}, state, {
                 tools: action.payload,
             });
         case ToolsActions.API_TOOLS_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                tools: action.payload,
+            });
         case ToolsActions.API_TOOL_SUCCESS:
             return Object.assign({}, state, {
                 tool: action.payload,
             });
         case ToolsActions.API_TOOL_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                tool: action.payload,
+            });
         case ProjectActions.API_PROJECT_SUCCESS:
             return Object.assign({}, state, {
                 project: action.payload,
             });
         case ProjectActions.API_PROJECT_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                project: action.payload,
+            });
 
         case AppActions.API_APP_SUCCESS:
             return Object.assign({}, state, {
                 app: action.payload,
             });
         case AppActions.API_APP_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                app: action.payload,
+            });
 
         case ProjectsActions.API_PROJECT_CREATE_SUCCESS:
             return Object.assign({}, state, {
                 createProjectResult: action.payload,
             });
         case ProjectsActions.API_PROJECT_CREATE_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                createProjectResult: action.payload,
+            });
 
         case AppActions.API_APP_CREATE_SUCCESS:
             return Object.assign({}, state, {
                 createAppResult: action.payload,
             });
         case AppActions.API_APP_CREATE_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                createAppResult: action.payload,
+            });
 
         case CasesActions.API_CASE_CREATE_SUCCESS:
             return Object.assign({}, state, {
                 createCaseResult: action.payload,
             });
         case CasesActions.API_CASE_CREATE_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                createCaseResult: action.payload,
+            });
 
         case AppActions.API_APP_DELETE_SUCCESS:
             return Object.assign({}, state, {
                 deleteAppResult: action.payload,
             });
         case AppActions.API_APP_DELETE_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                deleteAppResult: action.payload,
+            });
 
         case AppActions.API_PROJECT_DELETE_SUCCESS:
             return Object.assign({}, state, {
                 deleteProjectResult: action.payload,
             });
         case AppActions.API_PROJECT_DELETE_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                deleteProjectResult: action.payload,
+            });
         case BreadCrumbMenuActions.SET_BREAD_CRUMB_MEU:
             return Object.assign({}, state, {
                 breadCrumbMenus: action.payload,
@@ -213,84 +234,110 @@ export function reduxResult(state = initialState, action) {
                 deleteCaseResult: action.payload,
             });
         case CasesActions.API_CASE_DELETE_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                deleteCaseResult: action.payload,
+            });
         case CasesActions.API_CASE_SUCCESS:
             return Object.assign({}, state, {
                 cas: action.payload,
             });
         case CasesActions.API_CASE_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                cas: action.payload,
+            });
         case CasesActions.API_CASES_SUCCESS:
             return Object.assign({}, state, {
                 cases: action.payload,
             });
         case CasesActions.API_CASES_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                cases: action.payload,
+            });
         case CasesActions.API_CASE_GROUP_DELETE_SUCCESS:
             return Object.assign({}, state, {
                 deleteCaseGroupResult: action.payload,
             });
         case CasesActions.API_CASE_GROUP_DELETE_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                deleteCaseGroupResult: action.payload,
+            });
         case CasesActions.API_CASE_GROUP_SUCCESS:
             return Object.assign({}, state, {
                 caseGroup: action.payload,
             });
         case CasesActions.API_CASE_GROUP_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                caseGroup: action.payload,
+            });
         case CasesActions.API_CASE_GROUPS_SUCCESS:
             return Object.assign({}, state, {
                 caseGroups: action.payload,
             });
         case CasesActions.API_CASE_GROUPS_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                caseGroups: action.payload,
+            });
 
         case CasesActions.API_CASE_CREATE_GROUPS_SUCCESS:
             return Object.assign({}, state, {
                 createCaseGroupResult: action.payload,
             });
         case CasesActions.API_CASE_CREATE_GROUPS_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                createCaseGroupResult: action.payload,
+            });
 
         case TasksActions.API_TASKS_SUCCESS:
             return Object.assign({}, state, {
                 tasks: action.payload,
             });
         case TasksActions.API_TASKS_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                tasks: action.payload,
+            });
 
         case TasksActions.API_TASK_SUCCESS:
             return Object.assign({}, state, {
                 task: action.payload,
             });
         case TasksActions.API_TASK_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                task: action.payload,
+            });
 
         case TasksActions.API_TASKS_RESULT_SUCCESS:
             return Object.assign({}, state, {
                 scanResults: action.payload,
             });
         case TasksActions.API_TASKS_RESULT_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                scanResults: action.payload,
+            });
         case TasksActions.API_TASKS_START_SUCCESS:
             return Object.assign({}, state, {
                 taskStartResult: action.payload,
             });
         case TasksActions.API_TASKS_START_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                taskStartResult: action.payload,
+            })
 
         case CasesActions.API_TOOL_CASES_SUCCESS:
             return Object.assign({}, state, {
                 toolCases: action.payload,
             });
         case CasesActions.API_TOOL_CASES_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                toolCases: action.payload,
+            });
         case TasksActions.API_RESULT_SUCCESS:
             return Object.assign({}, state, {
                 result: action.payload,
             });
         case TasksActions.API_RESULT_FAILED:
-            return state;
+            return Object.assign({}, state, {
+                result: action.payload,
+            });
         default:
             return state;
     }
