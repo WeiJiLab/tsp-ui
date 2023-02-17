4e84be0876bbec03501ad9d9e2398d61fd5f31a1
wenwenzhang <omissyouyou@gmail.com>
2023-02-17 10:09:16 +0800

    add ScanningPage component

--- src/pages/scanningPage/ScanningPage.tsx
+++ src/pages/scanningPage/ScanningPage.tsx
@@ -1,12 +1,93 @@
 import React from 'react';
-
 import { MainLayout } from '../../components';
-// import { PageProject } from './pageProject';
+import { message, Upload, UploadProps, Steps, List, Typography } from 'antd';
+import {
+  InboxOutlined,
+  LoadingOutlined,
+  SmileOutlined,
+  SolutionOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import styles from './ScanningPage.module.css';
+
+const { Dragger } = Upload;
+
+const props: UploadProps = {
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
+};
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
 
 export const ScanningPage: React.FC = () => {
   return (
     <MainLayout>
-      {/* <PageProject /> */}
+      <Dragger {...props}>
+        <p className='ant-upload-drag-icon'>
+          <InboxOutlined />
+        </p>
+        <p className='ant-upload-text'>Click or drag file to this area to upload</p>
+        <p className='ant-upload-hint'>
+          Support for a single or bulk upload. Strictly prohibit from uploading company data or
+          other band files
+        </p>
+      </Dragger>
+      <Steps
+        className={styles.steps}
+        items={[
+          {
+            title: 'Login',
+            status: 'finish',
+            icon: <UserOutlined />,
+          },
+          {
+            title: 'Verification',
+            status: 'finish',
+            icon: <SolutionOutlined />,
+          },
+          {
+            title: 'Pay',
+            status: 'process',
+            icon: <LoadingOutlined />,
+          },
+          {
+            title: 'Done',
+            status: 'wait',
+            icon: <SmileOutlined />,
+          },
+        ]}
+      />
+      <List
+        header={<div>Header</div>}
+        footer={<div>Footer</div>}
+        bordered
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item>
+            <Typography.Text mark>[ITEM]</Typography.Text> {item}
+          </List.Item>
+        )}
+      />
     </MainLayout>
   );
 };
